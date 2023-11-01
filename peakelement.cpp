03c964f897a2f03619965a51779caf322eacfc63
NirmalaPathivada <143801923+NirmalaPathivada@users.noreply.github.com>
2023-11-01 17:23:02 +0530

    Add files via upload

---
+++ peakelement.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int peakelement(int arr[],int n){
+    int f=0,l=2,m=1;
+    if(arr[0]>arr[1])
+            return 0;
+    if(arr[n-1]>arr[n-2])
+            return n-1;
+    while(l<n){
+        if(arr[f]<arr[m] && arr[m]>arr[l]){
+          return arr[m];
+        }
+        l++;
+        m++;
+        f++;
+    }
+     return -1;
+}
+int main(){
+   int arr[5]={8,5,7,6,9};
+   int res= peakelement(arr,5);
+   if (res != -1) {
+        cout << "Peak element is: " << res << endl;
+    } else {
+        cout << "No peak element found." << endl;
+    }
+
+   return 0;
+
+}*/
+int peakelement(int arr[],int n){
+    
+}
